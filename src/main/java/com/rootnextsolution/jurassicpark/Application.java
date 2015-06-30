ea5ab88ad46214b6418d868c333162f8ed16aef0
Tahmid Tanzim <tahmid.tanzim@rootnext.com>
2015-06-30 12:47:17 +0600

    Initialize new Jurassic park

---
+++ src/main/java/com/rootnextsolution/jurassicpark/Application.java
@@ -0,0 +1,14 @@
+package com.rootnext.jurassicpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tahmid.tanzim on 6/30/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
